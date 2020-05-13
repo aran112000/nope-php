fe31710420e47d7df4a4cf68e717042a7c1a9986
Aran Reeks <cdtreeks@gmail.com>
2020-05-13 17:54:40 +0100

    Added support for setting private settings using a settings.ini file in the root (git excluded), will be created from a template file when installed via composer though

---
+++ src/Config/Setting.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Config;
+
+/**
+ * Class Setting
+ *
+ * @package App\Config
+ */
+class Setting
+{
+
+    const SETTING_FILE = 'settings.ini';
+
+    /**
+     * @param string $block
+     * @param string $value
+     *
+     * @return mixed|null
+     */
+    public static function get($block, $value)
+    {
+        static $settings;
+
+        if ($settings === null) {
+            $settings = [];
+            if ($tmpSettings = parse_ini_file(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . static::SETTING_FILE, true)) {
+                $settings = $tmpSettings;
+            }
+        }
+
+        if (isset($settings[$block][$value])) {
+            return $settings[$block][$value];
+        }
+
+        return null;
+    }
+}
