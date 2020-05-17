4f856406507492554a8207c25e9a0ddb3b72a518
Aran Reeks <cdtreeks@gmail.com>
2020-05-17 15:45:00 +0100

    Refactor the user agent whitelist to run from the settings instead of a hardcoded array

--- src/Config/UserAgent.php
+++ src/Config/UserAgent.php
@@ -4,54 +4,18 @@ namespace App\Config;
 
 class UserAgent
 {
-    /**
-     * A list of user agent strings to be whitelisted from this rule, these
-     * are matched case insensitively as a partial match so things like
-     * Googlebot will match a number of their variants (images, video,
-     * news...) for example.
-     *
-     * @var string[][] $whitelistedUserAgents
-     */
-    protected static $whitelistedUserAgents = [
-        'Google' => [
-            'Googlebot',
-            'Mediapartners-Google',
-            'AdsBot-Google',
-        ],
-        'Bing' => [
-            'bingbot',
-        ],
-        'Yahoo' => [
-            'Slurp',
-        ],
-        'DuckDuckGo' => [
-            'DuckDuckBot',
-        ],
-        'Baiduspider' => [
-            'Baiduspider',
-        ],
-        'Yandex' => [
-            'YandexBot',
-        ],
-        'Facebook' => [
-            'facebookexternalhit',
-        ],
-        'Alexa' => [
-            'ia_archiver',
-        ],
-        'LinkedIn' => [
-            'LinkedInBot',
-        ],
-    ];
 
     /**
+     * User agent strings to be whitelisted are matched case insensitively as a partial match so things like Googlebot
+     * will match a number of their variants (images, video, news...) for example.
+     *
      * @param string $userAgentStringToValidate
      *
      * @return string|false
      */
     public static function isTrusted($userAgentStringToValidate)
     {
-        foreach (static::$whitelistedUserAgents as $userAgentDescription => $userAgentVersions) {
+        foreach (Setting::get('Whitelist', 'IPs') as $userAgentDescription => $userAgentVersions) {
             foreach ($userAgentVersions as $agentVersion) {
                 if (stristr($userAgentStringToValidate, $agentVersion)) {
                     return $userAgentDescription;
