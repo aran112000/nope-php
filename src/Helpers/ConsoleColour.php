4b201edf393b48ba3a5f26c9aef8bdb8f86630e9
Aran Reeks <cdtreeks@gmail.com>
2020-05-17 12:56:35 +0100

    PHPCS fixes

--- src/Helpers/ConsoleColour.php
+++ src/Helpers/ConsoleColour.php
@@ -2,11 +2,15 @@
 
 namespace App\Helpers;
 
+/**
+ * Class ConsoleColour
+ *
+ * @package App\Helpers
+ */
 class ConsoleColour
 {
 
     const TEXT_GREEN = '0;32'; // Okay
     const TEXT_BLUE = '0;36';  // Warning
     const TEXT_RED = '0;31';   // Error
-
 }
